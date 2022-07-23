fa970b5c67ad53e9d6211e394680a44f880cddf3
shintarou37 <m.sintarou37@gmail.com>
2022-07-23 11:17:54 +0900

    [Add] フォームのレイアウト作成

--- next_plactice_ts/pages/components/list.tsx
+++ next_plactice_ts/pages/components/list.tsx
@@ -6,6 +6,15 @@ export default function List(props: any) {
   const { data } = useSWR('/test_data/data.json', fetcher)
   return (
     <div>
+      <form>
+      <h3>フォーム</h3><br></br>
+        <label>タイトル</label><br></br>
+          <input name="title"></input><br></br>
+          <label>内容</label><br></br>
+          <textarea name="content"></textarea><br></br>
+          <input type="submit" value="送信"></input>
+      </form><br></br><br></br>
+      <h3>一覧</h3>
         <table className="table table-dark">
           <tbody>
             {data != undefined ? data.data.map((value, key)=> (
@@ -16,6 +25,7 @@ export default function List(props: any) {
             )) : <tr><th></th><td>no data.</td><td></td></tr>}
           </tbody>
         </table>
+
     </div>
   )
 }
