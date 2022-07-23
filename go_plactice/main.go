d654a0cdbc684180addb5fc5b45f5e3dd1285d4b
shintarou37 <m.sintarou37@gmail.com>
2022-07-23 21:42:23 +0900

    [Add] JSONを返却する

--- go_plactice/main.go
+++ go_plactice/main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+    "encoding/json"
 )
+type Data1 struct {
+	Title    string `json:"title"`
+	Message  string `json:"content"`
+}
 
 func main() {
     fmt.Println("Hello!");
@@ -12,4 +17,19 @@ func main() {
 }
 func handler(w http.ResponseWriter, r *http.Request){
     fmt.Println("handler!");
+    var data1 = Data1{}
+    data1.Title = "sample1"
+    data1.Message = "hello, sample1"
+
+    // jsonエンコード
+    outputJson, err := json.Marshal(&data1)
+    if err != nil {
+        panic(err)
+    }
+
+    // jsonヘッダーを出力
+    w.Header().Set("Content-Type", "application/json")
+
+    // jsonデータを出力
+    fmt.Fprint(w, string(outputJson))
 }
