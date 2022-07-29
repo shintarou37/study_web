7448d912eb4e33122d2c3c62ad9900143f1f03f7
shintarou37 <m.sintarou37@gmail.com>
2022-07-29 10:15:29 +0900

    [Go] リファクタリング

--- go_plactice/main.go
+++ go_plactice/main.go
@@ -11,25 +11,28 @@ type Data1 struct {
 }
 
 func main() {
-    fmt.Println("Hello!");
+    fmt.Println("Start!");
     http.HandleFunc("/", handler);
     http.ListenAndServe(":8080", nil)
+    fmt.Println("End!");
+
 }
 func handler(w http.ResponseWriter, r *http.Request){
+    fmt.Println("GOが呼び出された")
     w.Header().Set("Access-Control-Allow-Origin", "*")
     var data1 = Data1{}
-    var data2 = Data1{"2title","2content"}
+    // var data2 = Data1{"2title","2content"}
     data1.Title = "sample1"
     data1.Content = "hello, sample1"
 
     // jsonエンコード
     outputJson, err := json.Marshal(&data1)
-    outputJson2, err2 := json.Marshal(&data2)
-    if err != nil || err2 != nil {
+    // outputJson2, err2 := json.Marshal(&data2)
+    if err != nil /*|| err2 != nil*/ {
         panic(err)
     }
-    fmt.Println(string(outputJson));
-    fmt.Println(string(outputJson2));
+    // fmt.Println(string(outputJson));
+    // fmt.Println(string(outputJson2));
     // jsonヘッダーを出力
     w.Header().Set("Content-Type", "application/json")
 
