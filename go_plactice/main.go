baa110fc2fc14b9f824495584b246cc403da89ad
shintarou37 <m.sintarou37@gmail.com>
2022-08-01 15:10:40 +0900

    [Go] スライスに複数の構造体を格納する

--- go_plactice/main.go
+++ go_plactice/main.go
@@ -22,11 +22,14 @@ func handler(w http.ResponseWriter, r *http.Request){
     w.Header().Set("Access-Control-Allow-Origin", "*")
     var datas = []Data1{}
     var data1 = Data1{}
+    var data2 = Data1{"smaple2", "hello, sample2"}
+    // fmt.Println(datas)
 
     data1.Title = "sample1"
     data1.Content = "hello, sample1"
     datas = append(datas, data1)
-    fmt.Println(datas)
+    datas = append(datas, data2)
+    // fmt.Println(datas)
     // jsonエンコード
     // outputJson, err := json.Marshal(&data1)
     dataput, err := json.Marshal(datas)
@@ -38,7 +41,7 @@ func handler(w http.ResponseWriter, r *http.Request){
     // fmt.Println(string(outputJson2));
     // jsonヘッダーを出力
     w.Header().Set("Content-Type", "application/json")
-
+    fmt.Println(datas)
     // jsonデータを出力
     fmt.Fprint(w, string(dataput))
     // fmt.Fprint(w, string(outputJson))
