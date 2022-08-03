e6fc748a353baff7b7d98c970ca830f06d5c1318
shintarou37 <m.sintarou37@gmail.com>
2022-08-03 09:31:06 +0900

    [GO]migrateファイルを1つにまとめる

--- go_plactice/migrate/migrate.go
+++ go_plactice/migrate/migrate.go
@@ -3,18 +3,41 @@ package main
 import (
 	"fmt"
     "gorm.io/gorm"
-    "01up"
-    "01down"
+    "gorm.io/driver/mysql"
 )
 type Data1 struct {
     gorm.Model
-	Title    string `json:"title"`
-	Content  string `json:"content"`
+	title    string `json:"title"`
+	content  string `json:"content"`
 }
 
 func main() {
     fmt.Println("Start migrate!");
-    // Up001()
-    01down.Down001()
+    up01()
+    down01();
     fmt.Println("End migrate!");
+}
+
+func up01() {
+    fmt.Println("Start 001_up!");
+    // dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+
+    db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+    if err != nil {
+		panic("failed to connect database")
+	}
+    // テーブル作成
+    db.AutoMigrate(&Data1{})
+    fmt.Println("End 001_up!");
+}
+func down01() {
+    fmt.Println("Start 001_down!");
+    // dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+    db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+    if err != nil {
+		panic("failed to connect database")
+	}
+    // テーブル削除
+    db.Migrator().DropTable(&Data1{})
+    fmt.Println("End 001_down!");
 }
