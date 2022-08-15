0b8bc3683b7a81c05d602dad8c6a18d6baa65674
shintarou37 <m.sintarou37@gmail.com>
2022-08-15 18:15:48 +0900

    [React] トップ画面へ戻るリンクを追加

--- next_plactice_ts/pages/detal.tsx
+++ next_plactice_ts/pages/detal.tsx
@@ -3,7 +3,10 @@ import Link from 'next/link'
 export default function Detail() {
   return (
     <div>
-      詳細
+      <h1>詳細画面</h1>
+      <Link href="/">
+          <a>トップ画面へ戻る</a>
+      </Link>
     </div>
   )
 }
