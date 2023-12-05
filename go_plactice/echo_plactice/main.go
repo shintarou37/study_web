26d6ca96e187d606e285fc29d1aef6e15a6d132a
shintarou37 <m.sintarou37@gmail.com>
2023-12-05 19:00:23 +0900

    check how to get query params and path

--- go_plactice/echo_plactice/main.go
+++ go_plactice/echo_plactice/main.go
@@ -1,27 +1,32 @@
 package main
 
 import (
-  "github.com/labstack/echo/v4"
-  "github.com/labstack/echo/v4/middleware"
-  "net/http"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
 )
 
 func main() {
-  // Echo instance
-  e := echo.New()
+	// Echo instance
+	e := echo.New()
 
-  // Middleware
-  e.Use(middleware.Logger())
-  e.Use(middleware.Recover())
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
-  // Routes
-  e.GET("/", hello)
+	// Routes
+	e.GET("/", hello)
+	e.GET("/users/:id", getUser)
 
-  // Start server
-  e.Logger.Fatal(e.Start(":1323"))
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
 }
 
 // Handler
 func hello(c echo.Context) error {
-  return c.String(http.StatusOK, "Hello, World!")
+	return c.String(http.StatusOK, "Hello, World!")
+}
+
+func getUser(c echo.Context) error {
+	return c.String(http.StatusOK, "team:" + c.Param("id") + ", member:" + c.QueryParam("member"))
 }
