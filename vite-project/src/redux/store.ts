554d8a3fb0603cda4acb52eeef5617300da188e1
shintarou37 <m.sintarou37@gmail.com>
2023-12-17 21:23:47 +0900

    update: add reducer in store

--- vite-project/src/redux/store.ts
+++ vite-project/src/redux/store.ts
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import counterSlice from './counterSlice';
 // configureStoreはstoreを簡単に作成できる関数
 export const store = configureStore({
-  reducer: {},
+  // storeにreducer（stateの状態を変更するための関数）を追加する
+  reducer: {
+    counter: counterSlice,
+  },
 })
