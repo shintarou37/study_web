19cc354a3c854e31520e6683ed37c2d828ebabbc
shintarou37 <m.sintarou37@gmail.com>
2023-12-17 21:54:45 +0900

    feat: increment func

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,15 +1,18 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
+import { increment } from './redux/counterSlice';
 
 function App() {
   // useSelectorは状態にアクセスできる
   // state.counterはstoreのcounterから取得してきている
   let count = useSelector((state: { counter: { value: number } }) => state.counter.value);
-
+  // dispatchはstoreに通知する
+  let dispatch = useDispatch();
   return (
     <>
       <div className='APP'>
         <p>counter: {count}</p>
+        <button onClick={ ()=> dispatch(increment()) }>➕</button>
       </div>
     </>
   )
