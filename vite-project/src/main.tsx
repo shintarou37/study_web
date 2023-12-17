7cfcbf4e26b171a36422b5e41a86cc28aa207d77
shintarou37 <m.sintarou37@gmail.com>
2023-12-17 21:15:16 +0900

    feat: add redux provider in App component

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {/* APPコンポーネントであればstoreを共有で使用できるように設定する */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
